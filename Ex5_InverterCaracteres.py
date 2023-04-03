a40078cb9fff5ab78dc48b9d1f4fb9813196e1d0
Carla-Cordeiro <carlacordeiroads@gmail.com>
2023-04-03 14:52:10 -0300

    exercicio 5

--- Ex5_InverterCaracteres.py
+++ Ex5_InverterCaracteres.py
@@ -3,3 +3,8 @@
 #a) Essa string pode ser informada através de qualquer entrada de sua preferência ou pode ser previamente definida no código;
 #b) Evite usar funções prontas, como, por exemplo, reverse;
 
+palavra = input('Digite a palavra que deseja inverter: ')
+
+string_invertida = palavra[::-1]
+
+print(f'Palavra original: {palavra}, Palavra invertida: {string_invertida}')
